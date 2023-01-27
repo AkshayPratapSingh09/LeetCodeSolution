80868f025f76ca3bfbbb6743c10c9b3e82dd9f31
Akshay <worklikeakshay@gmail.com>
2023-01-27 20:54:26 +0530

    Solved Subset

--- subset.py
+++ subset.py
@@ -1,17 +1,18 @@
-nums = [1,2,3]
-subsets = [[]]
+# nums = [1,2,3]
+# subsets = [[]]
 
-for num in nums:
-    # print("Case here",num)
-    # print("Subset right  now ->",len(subsets))
+# for num in nums:
+#     print("Case here",num)
+#     print("Subset right  now ->",len(subsets))
 
-    for i in range(len(subsets)):
-        print("index here ->",i)
-        print("subsets",subsets)
-        currSubset = subsets[i]
-        # print("Index ->",currSubset)
-        case = currSubset + [num]
-        subsets.append(case)
-        print("subsets",subsets)
+#     for i in range(len(subsets)):
+#         print("index here ->",i)
+#         print("subsets",subsets)
+#         currSubset = subsets[i]
+#         print("Index ->",currSubset)
+#         case = currSubset + [num]
+#         subsets.append(case)
+#         print("subsets",subsets)
 # print( subsets)
 # print([1,2,3] + [56])
+
---
+++ subset_approach_1.py
@@ -0,0 +1,11 @@
+
+arr = [1,2,3]
+
+res = [[]]
+
+for i in res:
+    for j in arr:
+        print(i)
+        i.append(j)
+        # print(curr)
+        res.append(i)
