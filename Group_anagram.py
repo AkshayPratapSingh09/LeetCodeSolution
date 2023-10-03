11cfa3f8ae8124b3a704a6f8b9b6710710f02e57
Akshay Pratap Singh <99736940+AkshayPratapSingh09@users.noreply.github.com>
2023-10-03 23:50:32 +0530

    Update Group_anagram.py

--- Group_anagram.py
+++ Group_anagram.py
@@ -3,6 +3,6 @@ word1 = ["tea", "ate"]
 # word2 = ["t", "a", "e"]
 word1.sort()
 # word2.sort()
-print(word1)
-# print(word2)
-# print(word1==word2)
+# print(word1)
+print(word2)
+print(word1==word2)
